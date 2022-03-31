98628954ba74b52d4873274e89bdca7454d33316
Joel Corey <joelcorey@fastmail.com>
2022-03-31 07:55:45 -0700

    youtube video stub out

--- components/HeaderSimple.js
+++ components/HeaderSimple.js
@@ -16,17 +16,66 @@ export default function Header({ pathKeys }) {
 				h-96
 				flex-none
 				text-white
-
 		`}
 		>
+			<div className='
+				absolute
+				text-4xl
+				mx-auto
+				z-10
+				right-4
+				bottom-2
+
+			'>
+				{
+					appSettings[pathKeys.agentVideo] &&
+					<iframe
+						width="315px"
+						height="177px"
+						src="https://www.youtube.com/embed/eTFHsRQ-DKM"
+						title="YouTube video player"
+						// eslint-disable-next-line react/no-unknown-property
+						frameborder="0"
+						allow="
+							accelerometer;
+							autoplay;
+							clipboard-write;
+							encrypted-media;
+							gyroscope;
+							picture-in-picture"
+						// eslint-disable-next-line react/no-unknown-property
+						allowfullscreen
+					/>
+				}
+
+
+			</div>
+			<div className='
+				absolute
+				text-3xl
+				z-10
+				left-1/2
+				transform -translate-x-1/2
+			'>
+				<div className='
+					flex
+					flex-col
+				'>
+					<div>{appSettings[pathKeys.title]}</div>
+					<div>{appSettings[pathKeys.subTitle]}</div>
+				</div>
+
+			</div>
+
 			{
 				appSettings[pathKeys.backgroundImage] &&
 				<div
 					className={`
-						relative
+						absolute
 						w-full
 						h-full
 						overflow-hidden
+						z-0
 					`}
 					style={{
 						backgroundImage    : `url(${appSettings[pathKeys.backgroundImage]})`,
@@ -34,18 +83,7 @@ export default function Header({ pathKeys }) {
 						backgroudSize      : 'cover'
 					}}
 				/>
-				}
-
-			<div className='
-				text-4xl
-			'>
-				{appSettings[pathKeys.title]}
-			</div>
-			<div className='
-				text-3xl
-			'>
-				{appSettings[pathKeys.subTitle]}
-			</div>
+			}
 		</div>
   );
 }
